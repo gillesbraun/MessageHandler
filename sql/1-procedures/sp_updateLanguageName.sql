7714f0fba4102d64e8bfc969848b30c77c12c5e1
Gilles Braun <braun.gillo@gmail.com>
2017-01-23 08:05:33 +0100

    sp_updateLanguageName: Removed unnecessary transaction and output params

--- sql/1-procedures/sp_updateLanguageName.sql
+++ sql/1-procedures/sp_updateLanguageName.sql
@@ -18,53 +18,11 @@ DELIMITER ??
 CREATE DEFINER = 'MsgHandler'@'localhost'
 PROCEDURE sp_updateLanguageName(
   IN i_idLanguage CHAR(2),
-  IN i_name       VARCHAR(100),
-  OUT o_code      INT UNSIGNED,
-  OUT o_message   VARCHAR(100))
+  IN i_name       VARCHAR(100))
   SQL SECURITY DEFINER
   BEGIN
-    DECLARE deadlockOccured INT DEFAULT 0;
-    DECLARE nbrOfAttempts INT DEFAULT 0;
 
-    SET o_code = 0;
-    SET o_message = "";
-
-    -- Start a loop to try the transaction 3 times
-    tra_loop: WHILE (nbrOfAttempts < 3) DO
-      BEGIN
-        -- Define an exit handler when deadlock occurs.
-        DECLARE deadlock CONDITION FOR 1213;
-        DECLARE EXIT HANDLER FOR deadlock
-        BEGIN
-          SET deadlockOccured = 1;
-          ROLLBACK;
-        END;
-        START TRANSACTION;
-
-        -- Do the action
-        UPDATE tblLanguage
-        SET dtName = i_name
-        WHERE idLanguage = i_idLanguage;
-
-        COMMIT;
-
-      END;
-      -- End of the block where deadlocks can occur
-
-      IF deadlockOccured = 0
-      THEN
-        -- Leave when no deadlock occured
-        LEAVE tra_loop;
-      ELSE
-        -- Try again if deadlock occured
-        SET nbrOfAttempts = nbrOfAttempts + 1;
-      END IF;
-    END WHILE tra_loop;
-
-    IF deadlockOccured = 1
-    THEN
-      CALL sp_getMsg(1, 'en', '3', @msg);
-      SET o_message = @msg;
-      SET o_code = 1213;
-    END IF;
+    UPDATE tblLanguage
+    SET dtName = i_name
+    WHERE idLanguage = i_idLanguage;
   END ??
