11f80ed19e8142e3c666babec350e638a1b9bc93
Gilles Braun <braun.gillo@gmail.com>
2017-01-27 10:38:26 +0100

    Removed default data

--- sql/2-after/00-defaultData.sql
+++ sql/2-after/00-defaultData.sql
@@ -1,8 +1,5 @@
 
 DELIMITER ;
-CALL sp_addUser('admin', '13371337', 1, @c, @msg);
-CALL sp_addUser('Angus', '1337', 0, @c, @msg);
-
 
 -- Default Values
 INSERT INTO tblLanguage (idLanguage, dtName, dtLocalizedName) VALUES
@@ -38,14 +35,3 @@ INSERT INTO tblMessageInLanguage (fiMessage, fiLanguage, dtMessageInLanguage) VA
   (3, 'en', 'Foreign key not found.'),
   (3, 'de', 'Fremdschlüssel nicht gefunden.'),
   (3, 'fr', 'Clé étrangère pas trouvé.');
-
-CALL sp_addOutput('admin', 'de', 'Admin Output to Logfile', @output, @c, @msg);
-CALL sp_addOutputLogfile(@output, '/tmp/adminmsghandler.log', @c, @msg);
-CALL sp_assignMessageToOutput(1, @output, @c, @m);
-
-CALL sp_addOutput('Angus', 'en', 'Default Logfile and email', @output, @c, @msg);
-CALL sp_addOutputLogfile(@output, '/tmp/msghandler.log', @c, @msg);
-CALL sp_addOutputEmail(@output, 'Log from MsgHandler', 'braun.gillo@gmail.com', @c, @msg);
-CALL sp_addOutputTwitter(@output, 'jLftnT7NI5p2iiwhvnQeKgDoZ', 'dnncZLjUQEtOSeCzZk0Rh7EJy2fY94zhhV6rvP0WW5NrtVorID',
-'88227924-upe2jQES0eIK7EyxK7qNcS1NGFj6B72GthFeKeFTy', 'kpPAziveUVy67F7PGyAoP87PcpeG9hIdUOwCjPqfNY5U8', @c, @msg);
-CALL sp_assignMessageToOutput(2, @output, @c, @m);
